af3c6613dc643e452b8d5de99de5dc9b10a62280
jerguslejko <jergus.lejko@gmail.com>
2016-09-28 15:06:15 +0100

    Fix typo

--- task2.py
+++ task2.py
@@ -1,6 +1,6 @@
 number = input("Please enter a whole number: ")
 
-if int(number) % 3 == 0: # determine wether the number is divisible by 3
+if int(number) % 3 == 0: # determine whether the number is divisible by 3
     print("Is a multiple of 3")
 else:
     print("Is not a multiple of 3")
